767d57b0e696aa880cdfda659b4846f25f9f7cf6
Brian Craycraft <craycraftb24@hotmail.com>
2020-06-20 00:11:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,9 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
+    binding.pry
   end
   
   def print_courses
@@ -21,6 +23,8 @@ class Scraper
   end
   
 end
+
+Scraper.new.get_page
 
 
 
